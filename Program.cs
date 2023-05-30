3bc00149bd3df92700d5e45c6664a5a5a6a94a89
Dazigion <82140291+Dazigion@users.noreply.github.com>
2023-05-30 14:53:22 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace MultipleRoblox
 		
         static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Welcome to Roblox Multi-Instance!\n");
+			Console.ForegroundColor = ConsoleColor.Green;
+            		Console.WriteLine(" Welcome to Roblox Multi-Instance!\n");
 			Thread.Sleep(300); //Mainly because of the astetic of "loading"
 			bool IsActive;
 			Mutex mutex = new Mutex(true, "ROBLOX_singletonMutex",out IsActive);
@@ -19,7 +19,7 @@ namespace MultipleRoblox
 			{
 				if (!IsActive) {
 					Console.ForegroundColor = ConsoleColor.White;
-					Console.Write("Please close all Launched Roblox Instances for this to work, thanks! \n\n");
+					Console.Write("Either its already running or you need to rejoin!");
 				}
 				mutex.WaitOne();
 			}
@@ -29,15 +29,12 @@ namespace MultipleRoblox
 			}
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Roblox Multi-Instance is now activated!\n");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("Originally made by MiningTcup, some credit to MainDabRblx, Modified by Dazigion");
 			Console.ForegroundColor = ConsoleColor.Red;
-			Console.Write("Please do not close this window\n\n");
-			Console.ForegroundColor = ConsoleColor.White;
-			Console.Write("Originally made by MiningTcup, some credit to MainDabRblx \n \n Modified by Dazigion\n\n--- NOTES ---\nYou may now open as many Roblox instances as you like, ");
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.Write("using seperate accounts");
-			Console.ForegroundColor = ConsoleColor.White;
-			Console.WriteLine(". If you close this window, all Roblox instances except for one will close. Make sure that you closed all Roblox instances before activating this.\n");
-			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("> You CANNOT use the same account\n> Closing this window causes to deactivate!\n");
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("https://github.com/Dazigion/Roblox-Multi-Instance/");
 			Thread.Sleep(-1);
 		
         }
